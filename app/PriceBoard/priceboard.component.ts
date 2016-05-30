bea9d1976e4b6c88b02e5674b0010a26b4b0ee17
Joe <foxr4@foxjazz.net>
2016-05-29 20:56:16 -0600

    nearly done

--- app/PriceBoard/priceboard.component.ts
+++ app/PriceBoard/priceboard.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {ItemTypes, ItemType, ItemTypeDescriptor} from '../EveItems/ItemTypes';
-import {PriceTypes, items} from './pricetypes';
+import {PriceTypes, items, PriceData, PriceBand} from './pricetypes';
 import {ISystemShort, ISystemShortDescriptor} from '../Regions/IRegions';
 import {TypeValidator} from '../Assets/typescript-dotnet/source/System/TypeValidator';
 import {EvePricingService} from './evepricing.service';
@@ -8,16 +8,19 @@ import 'rxjs/Rx';
 @Component({
   selector: 'sel-pb',
   templateUrl: 'app/PriceBoard/priceboard.component.html',
-  //styleUrls: ['app/PriceBoard/canvas.css'],
+  styleUrls: ['app/PriceBoard/canvas.css'],
   providers: [EvePricingService]
 })
 export class PriceBoardComponent implements OnInit {
   public selSystems: Array<ISystemShort>;
   public selEveItems: Array<ItemType>;
   public resItems: Array<items>;
+  public prices: Array<PriceData>;
+  public priceBandA: Array<PriceBand>;
   constructor(private evePricingService: EvePricingService) { }
   ngOnInit() {
-
+    this.priceBandA = new Array<PriceBand>();
+    document.getElementById('noData').hidden = true;
     this.selSystems = new Array<ISystemShort>();
     let restry = JSON.parse(localStorage.getItem('Systems'));
     let first = false;
@@ -26,6 +29,7 @@ export class PriceBoardComponent implements OnInit {
     if (first && isd.isSubsetOf(restry)) {
       this.selSystems = restry;
     } else {
+      document.getElementById('noData').hidden = false;
       return;
     }
       let res = localStorage.getItem('SelEveItems');
@@ -35,26 +39,63 @@ export class PriceBoardComponent implements OnInit {
              this.selEveItems = restry;
          }
         else {  
-            this.selEveItems = new Array<ItemType>(); 
+            document.getElementById('noData').hidden = false;
             } 
     this.DoAllSelections();
   }
   refreshData() {
     this.DoAllSelections();
   }
-  private callPriceData(regionid: string, itemhref: string) {
+  
+  private aggItems(region: string, itemname: string,data: Array<items>){
+    let filtered: Array<items>;
+    let i = 0;
+    let pp = new Array<PriceData>();
+    for(i = 0; i < data.length; i++)
+    {
+      if(data[i].buy === false){
+        let p: PriceData = new PriceData();
+        p.duration = data[i].duration;
+        p.price = data[i].price;
+        p.volume = data[i].volume;
+        p.range = data[i].range;
+        p.issued = data[i].issued;
+        p.location = data[i].location.name;
+        pp.push(p);
+      }
+    }
+    // NEW INTERFACE OBJECT
+    let npb: PriceBand = {
+      region: region,
+      itemname: itemname,
+      pricedata: pp
+    };
+    this.priceBandA.push(npb);
+  }
+  
+  private callPriceData(region: string, itemname: string,regionid: string, itemhref: string) {
     this.evePricingService.getPriceData(regionid, itemhref).subscribe(res => {
-      this.resItems = res.items;
-      console.log(this.resItems);
+      if(res.items.length > 0)
+        this.aggItems(region, itemname, res.items);
     },
       err => console.log('Something went wrong:' + err.message));
   }
   private DoAllSelections() {
     let isys = 0;
     let iitem = 0;
+    try {
+      let x = this.selSystems.length + this.selEveItems.length;
+      if(x === 0){
+        document.getElementById('noData').hidden = false;
+        return;  
+      } 
+    } catch (error) {
+      document.getElementById('noData').hidden = false;
+      return;
+    }
     for (isys = 0; isys < this.selSystems.length; isys++) {
       for (iitem = 0; iitem < this.selEveItems.length; iitem++) {
-        this.callPriceData(this.selSystems[isys].regionid, this.selEveItems[iitem].type.href);
+        this.callPriceData(this.selSystems[isys].region,this.selEveItems[iitem].type.name,this.selSystems[isys].regionid, this.selEveItems[iitem].type.href);
       }
     }
   }
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,51 +1,51 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {RegionComponent} from './regions/region.component';
 import {ItemComponent} from './EveItems/item.component';
 import {PriceBoardComponent} from './PriceBoard/priceboard.component';
-//import {testComponent} from './PriceBoard/test.component';
-//import {HTTP_PROVIDERS} from '@angular/http';
-//import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/router-deprecated';
-
+import {HelpComponent} from './Help/help.component';
 import 'rxjs/Rx';
+
 @Component({
     selector: 'my-app',
     template: `<h1>My First Angular 2 App</h1>
     <nav>
+        <a (click)="menuitem('help')">Help</a>
         <a (click)="menuitem('region')">Region</a>
         <a (click)="menuitem('items')">Items</a>
-        <a (click)="menuitem('tpb')">test Price Board</a>
         <a (click)="menuitem('pb')">Price Board</a>
     </nav>
+    <div id="help">
+        <sel-help> loading test </sel-help>
+    </div>
     <div id="region">
         <sel-region> loading test </sel-region>
     </div>
     <div id="items">
         <sel-items> loading items </sel-items>
     </div>
-    <div id="tpb">
-        <sel-testboard> loading canvas </sel-testboard>
-    </div>
+   
     <div id="pb">
         <sel-pb> loading canvas </sel-pb>
     </div>
     `,
     styleUrls: ['app/app.css', 'app/assets/bootstrap/dist/css/bootstrap.min.css'],
-    directives: [RegionComponent, ItemComponent, PriceBoardComponent],
+    directives: [RegionComponent, ItemComponent, PriceBoardComponent, HelpComponent],
   //  providers: [HTTP_PROVIDERS]
 })
 
-export class AppComponent {
-    public region: string = 'hidden';
-    
-    public menuitem = function(itm: string){
+export class AppComponent implements OnInit {
+    constructor(){}
+    public menuitem(itm: string){
                 document.getElementById('items').hidden = true;
                 document.getElementById('pb').hidden = true;
-                document.getElementById('tpb').hidden = true;
                 document.getElementById('region').hidden = true;
+                  document.getElementById('help').hidden = true;
         switch(itm){
+            case 'help': {
+                document.getElementById('help').hidden = false;
+            }
+            break;
             case 'region': {
-                /*this.region = 'visible';
-                this.items = 'hidden';*/
                 document.getElementById('region').hidden = false;
             }
             break;
@@ -57,12 +57,9 @@ export class AppComponent {
                  document.getElementById('pb').hidden = false;
             }
             break;
-            case 'tpb': {
-                 document.getElementById('tpb').hidden = false;
-            }
-            break;
         }
     }
-    
-    
+    ngOnInit() {
+        this.menuitem('region');
+    }
  }
